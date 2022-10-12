5d63642031e40fc5b927be199878d66cb8e5289f
Jeremy Twidt <jtwidt@gmail.com>
2022-10-12 17:50:47 -0500

    male 40-44 seeds complete

---
+++ seeders/20221012222933-Male40thru44Plank.js
@@ -0,0 +1,148 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('ScoreCharts', [
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 195,
+        upperLimit: 900,
+        points: 20.0,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 190,
+        upperLimit: 194,
+        points: 19.7,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 185,
+        upperLimit: 189,
+        points: 19.3,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 179,
+        upperLimit: 184,
+        points: 18.9,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 172,
+        upperLimit: 178,
+        points: 18.5,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 165,
+        upperLimit: 171,
+        points: 18.0,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 145,
+        upperLimit: 164,
+        points: 16.7,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 125,
+        upperLimit: 144,
+        points: 15.3,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 105,
+        upperLimit: 124,
+        points: 14.0,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 85,
+        upperLimit: 104,
+        points: 12.7,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 65,
+        upperLimit: 84,
+        points: 11.3,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 45,
+        upperLimit: 64,
+        points: 10.0,
+      },
+      {
+        exerciseName: 'Forearm Plank Time (mm:ss)',
+        exerciseGroup: 'Core',
+        gender: 'Male',
+        lowerAge: 35,
+        upperAge: 39,
+        lowerLimit: 0,
+        upperLimit: 44,
+        points: 0.0,
+      },
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
