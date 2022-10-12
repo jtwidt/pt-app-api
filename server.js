1dcf088d6bfdc3121682cb831d0c78babaca6157
Jeremy Twidt <jtwidt@gmail.com>
2022-10-11 19:25:04 -0500

    db initial configuration

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 const cors = require('cors');
 // const db = requires('./models/index');
 
-// const routes = require('/routes');
+const routes = require('/routes');
 
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.use('/', routes);
+app.use('/', routes);
 
 // db.sequelize.sync();
 
