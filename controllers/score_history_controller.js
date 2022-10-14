fcf08a141df72b8c19171413a12b2100a1bdb540
Jeremy Twidt <jtwidt@gmail.com>
2022-10-14 16:05:02 -0500

    basic score history controller and routes complete

---
+++ controllers/score_history_controller.js
@@ -0,0 +1,87 @@
+const {
+  ScoreHistory,
+  User,
+  ScoreChart
+} = require('../models');
+const {getAge} = require('../utilities/helpers');
+const {Op} = require("sequelize");
+
+const createHistoryEntry = async (req, res) => {
+  try {
+    const {userId, exerciseName, value, testDate, isDiagnostic} = req.body;
+    const user = await User.findByPk(userId);
+    const userGender = user.gender;
+    const userAge = getAge(user.birthDate);
+    const exercise = await ScoreChart.findOne({
+      where: {
+        gender: userGender,
+        exerciseName: exerciseName,
+        lowerAge: {
+          [Op.lte]: userAge
+        },
+        upperAge: {
+          [Op.gte]: userAge
+        },
+        value: {
+          [Op.between]: [lowerLimit, upperLimit]
+        }
+      }
+    });
+    const newHistory = {
+      value,
+      userId,
+      exerciseId: exercise.id,
+      testDate,
+      isDiagnostic,
+    };
+    const history = ScoreHistory.create(newHistory);
+    return res.status(201).json({history});
+
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+}
+
+const getAllUserHistory = async (req, res) => {
+  try {
+    const {userId} = req.params;
+    const histories = await ScoreHistory.findAll({
+      where: {
+        userId: userId
+      },
+      include: [ScoreChart]
+    });
+    return res.status(200).json({histories});
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+}
+
+const getUserHistoryByDate = async (req, res) => {
+  try {
+    const {userId} = req.params;
+    const {startDate, endDate} = req.body;
+    const histories = await ScoreHistory.findAll({
+      where: {
+        userId: userId,
+        testDate: {
+          [Op.between]: [startDate, endDate]
+        }
+      },
+      include: [ScoreChart]
+    });
+    return res.status(200).json({histories});
+  } catch (err) {
+    return res.status(400).json({error: err.message});
+  }
+}
+
+module.exports = {
+  createHistoryEntry,
+  getAllUserHistory,
+  getUserHistoryByDate,
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const {Router} = require('express');
-// const controllers = require('../controllers');
+const historyController = require('../controllers/score_history_controller');
 const router = Router();
 
 router.get('/', (req, res) => {
   res.send('Bork bork bork');
-})
+});
+
+router.post('/history', historyController.createHistoryEntry);
+router.get('/history/:id', historyController.getAllUserHistory);
+router.get('/history/:id/dates', historyController.getUserHistoryByDate);
 
 module.exports = router;
